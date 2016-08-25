1b28078143a4d9f6b3d60bf08c5413e7f38b0ddd
Brandon Lim <brandondlim@gmail.com>
2016-08-24 21:11:07 -0700

    Implemented Carousel Buttons

--- js/script.js
+++ js/script.js
@@ -2,8 +2,43 @@ var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide,10000);
 
+var slideButtonsContainer = document.getElementById("slide-buttons");
+
+function addSlideButton(index) {
+    var newSlideButton = document.createElement('span');
+    newSlideButton.setAttribute('class', 'slideButton');
+
+    var functionCall = 'changeToSlide(' + index + ')';
+    newSlideButton.setAttribute('onclick', functionCall);
+
+    slideButtonsContainer.appendChild(newSlideButton);
+}
+
+for (i = 0; i < slides.length; i++) {
+    addSlideButton(i);
+}
+
+var slideButtons = document.querySelectorAll('#slide-buttons .slideButton');
+
+if (slides.length > 0) {
+    slideButtons[0].className = 'slideButton activeButton';
+}
+
+function changeToSlide(index) {
+    index = index % slides.length;
+
+    if (index !== currentSlide) {
+        slides[currentSlide].className = 'slide';
+        slideButtons[currentSlide].className = 'slideButton'
+
+        currentSlide = index;
+
+        slides[currentSlide].className = 'slide showing';
+        slideButtons[currentSlide].className = 'slideButton activeButton'
+    }
+}
+
 function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
+    newSlide = (currentSlide+1)%slides.length;
+    changeToSlide(newSlide);
 }
